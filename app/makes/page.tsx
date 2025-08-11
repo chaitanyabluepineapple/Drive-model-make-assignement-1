66ded65ec72145d5cf48d608f18f640686cc9d94
Chaitanya Mohite <92073516+chaitanyabluepineapple@users.noreply.github.com>
2025-08-11 20:50:56 +0530

    -Update metadata in layout and page components to add page title dynamically

--- app/makes/page.tsx
+++ app/makes/page.tsx
@@ -1,7 +1,13 @@
 import { fetchMakesServerSide } from "../hooks/fetchMakesSsr";
 import SSRMakesClient from "../components/SSRMakeDropdown";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "SSR Vehicle Make & Model Viewer",
+  description: "Explore vehicle makes and models with server-side rendering.",
+};
 
 export default async function SSRMakesPage() {
     const makes = await fetchMakesServerSide();
